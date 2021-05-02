e11e37117c93e3ee99b6c33d32938a05154b4bc4
Vlad Ushakevich <vlad_ushak16@tut.by>
2021-05-02 23:56:46 +0300

    script1.sh

---
+++ script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd copy; # заходим в папку
+mkdir dog1 cat2 dog2; # создать 3 папки
+cd dog # вошли в папку
+touch int1.txt int2.txt int3.txt into1.json into2.json # создать 5 файлов
+mkdir -p ~ / papka1 papka2 papka3   # создали 3 папки
+ls -l # вывели содержимое списка папок
+mv dog / int3.txt dog / into2.json papka1   # переместили 2 файла в другую папку
